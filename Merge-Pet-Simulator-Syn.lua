ca26f87210a5d2cdc365fd2597524a4b7a0e00ac
RobloxAvatar <85132986+RobloxAvatar@users.noreply.github.com>
2022-12-16 23:08:41 +0100

    Create Merge-Pet-Simulator-Syn.lua

--- Merge-Pet-Simulator-Syn.lua
+++ Merge-Pet-Simulator-Syn.lua
@@ -1,44 +1,42 @@
-function script()
-    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 
-    local Window = Library.CreateLib("Merge Pet Simulator - Synapse X", "Serpent")
+local Window = Library.CreateLib("Merge Pet Simulator - Synapse X", "Serpent")
 
-    local MainTab = Window:NewTab("Main")
+local MainTab = Window:NewTab("Main")
 
-    local MainSection = MainTab:NewSection("Main")
+local MainSection = MainTab:NewSection("Main")
 
-    getgenv().AutoClickBestPet = false
-    getgenv().AutoMerge = false
+getgenv().AutoClickBestPet = false
+getgenv().AutoMerge = false
 
-    MainSection:NewToggle("Auto Click Best Pet", "", function(state)
-        getgenv().AutoClickBestPet = state
-    end)
+MainSection:NewToggle("Auto Click Best Pet", "", function(state)
+    getgenv().AutoClickBestPet = state
+end)
 
-    MainSection:NewToggle("Auto Merge", "", function(state)
-        getgenv().AutoMerge = state
-    end)
+MainSection:NewToggle("Auto Merge", "", function(state)
+    getgenv().AutoMerge = state
+end)
 
-    MainSection:NewButton("Complete Obby", "3 minutes delay!", function()
-        game:GetService("ReplicatedStorage").Remotes.obby_done:FireServer()
-    end)
+MainSection:NewButton("Complete Obby", "3 minutes delay!", function()
+    game:GetService("ReplicatedStorage").Remotes.obby_done:FireServer()
+end)
 
-    while wait() do
-        if getgenv().AutoClickBestPet then
-            local petNames = {}
-            if workspace.Plots[game.Players.LocalPlayer.Name.."_Plot"]:FindFirstChild("ID", true) then
-                for i,v in pairs(workspace.Plots[game.Players.LocalPlayer.Name.."_Plot"]:GetChildren()) do
-                    if tonumber(v.Name) ~= nil then
-                        if not table.find(petNames, tonumber(v.Name)) then
-                            table.insert(petNames, tonumber(v.Name))
-                        end
+while wait() do
+    if getgenv().AutoClickBestPet then
+        local petNames = {}
+        if workspace.Plots[game.Players.LocalPlayer.Name.."_Plot"]:FindFirstChild("ID", true) then
+            for i,v in pairs(workspace.Plots[game.Players.LocalPlayer.Name.."_Plot"]:GetChildren()) do
+                if tonumber(v.Name) ~= nil then
+                    if not table.find(petNames, tonumber(v.Name)) then
+                        table.insert(petNames, tonumber(v.Name))
                     end
                 end
-                if workspace.Plots[game.Players.LocalPlayer.Name.."_Plot"]:FindFirstChild("ID", true) then
-                    if petNames[1] ~= nil then
-                        if table.unpack(petNames) ~= nil then
-                            if math.max(table.unpack(petNames)) ~= nil then
-                                game:GetService("ReplicatedStorage").Remotes.mouse_down:FireServer(workspace.Plots[game.Players.LocalPlayer.Name.."_Plot"]:FindFirstChild(math.max(table.unpack(petNames))))
-                            end
+            end
+            if workspace.Plots[game.Players.LocalPlayer.Name.."_Plot"]:FindFirstChild("ID", true) then
+                if petNames[1] ~= nil then
+                    if table.unpack(petNames) ~= nil then
+                        if math.max(table.unpack(petNames)) ~= nil then
+                            game:GetService("ReplicatedStorage").Remotes.mouse_down:FireServer(workspace.Plots[game.Players.LocalPlayer.Name.."_Plot"]:FindFirstChild(math.max(table.unpack(petNames))))
                         end
                     end
                 end
@@ -55,10 +53,4 @@ function script()
             end
         end
     end
-end
-
-if syn then
-    script()
-else
-    game.Players.LocalPlayer:Kick("You are not using Synapse X")
 end
